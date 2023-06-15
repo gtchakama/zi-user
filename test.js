8cca8049aed3a3634b44d9e062a48f0e19bfda9b
George T Chakama <37905683+gtchakama@users.noreply.github.com>
2023-06-15 12:47:22 +0200

    Revert "Revert "Added new username, converted arrays into 1 object""
    This reverts commit 7af76b284b795d8dc58b2882f4ac705ba38ee272.

---
+++ test.js
@@ -0,0 +1,6 @@
+const getZiUser = require("zi-user");
+
+const user = getZiUser();
+
+console.log(user);
+// Output: { name: 'Emma', surname: 'Clark', email: 'emma.clark2732@example.com', password: 'imzqcvkw' }
